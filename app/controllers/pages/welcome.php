2847b13c8874dfb49254ad00e05d70b31debfef3
Khai Rahman <khairahmanplus@gmail.com>
2020-10-14 11:26:14 +0800

    Tambah error handling pada pencarian layout

--- app/controllers/pages/welcome.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-view('app', 'welcome');
--- app/function.php
+++ app/function.php
@@ -4,8 +4,13 @@ function view(string $layout_name, string $view_name, $data = [])
 {
     extract($data);
 
-    include APP_DIRECTORY . "/views/layouts/{$layout_name}.view.php";
+    $layout_path = APP_DIRECTORY . "/views/layouts/{$layout_name}.view.php";
 
+    if (! file_exists($layout_path)) {
+        throw new Exception("Layout pada direktori {$layout_path} tidak wujud.");
+    }
+
+    include $layout_path;
     die();
 }
 
