0e0a0fdf361f2c9fc05981c1a5bb93f3c673ca1f
Khai Rahman <khairahmanplus@gmail.com>
2020-10-14 14:59:12 +0800

    Tambah proses log-masuk

--- app/controllers/auth/log-masuk.php
+++ app/controllers/auth/log-masuk.php
@@ -30,4 +30,36 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $errors_bag['emel'] = 'Emel yang diberikan tidak wujud di dalam pangkalan data.';
         }
     }
+    
+
+    if (! empty($_POST['emel'] && ! $_POST['kata_laluan'])) {
+        $sql_statement = 'SELECT * FROM pengguna WHERE emel = :emel';
+        $pdo_statement = $pdo->prepare($sql_statement);
+        $pdo_statement->bindValue(':emel', $_POST['emel']);
+        $pdo_statement->execute();
+        $user = $pdo_statement->fetch();
+
+        if (! password_verify($_POST['kata_laluan'], $user->password)) {
+            $errors_bag['emel'] = 'Medan emel dan kata laluan tidak sepadan.';
+        }
+    }
+
+    if (count($errors_bag) > 0) {
+        view('app', 'auth/log-masuk', [
+            'errors_bag' => $errors_bag
+        ]);
+    }
+
+    // Generate new session ID
+    session_regenerate_id();
+    // Cipta satu key unik berdasarkan session ID
+    $key = 'web_login_' . session_id();
+    // Simpan id pengguna dengan menggunakan key unik
+    $_SESSION[$key] = $user->id;
+    // Notifikasi
+    $_SESSION['notifikasi'] = 'Anda berjaya log masuk ke dalam aplikasi.';
+    // Redirect kepada /home
+    $url = url('/home');
+    header("Location: {$url}");
+    die();
 }
