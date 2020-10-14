74f71a3704fbb683a0bdb972541c264eb4a9fdc1
Khai Rahman <khairahmanplus@gmail.com>
2020-10-14 12:38:12 +0800

    Tambah validation pada log-masuk

--- app/controllers/auth/log-masuk.php
+++ app/controllers/auth/log-masuk.php
@@ -23,12 +23,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $sql_statement = 'SELECT EXISTS(SELECT * FROM pengguna WHERE emel = :emel) AS existence';
         $pdo_statement = $pdo->prepare($sql_statement);
         $pdo_statement->bindValue(':emel', $_POST['emel']);
+        $pdo_statement->execute();
         $exists = $pdo_statement->fetchColumn();
 
         if (! $exists) {
             $errors_bag['emel'] = 'Emel yang diberikan tidak wujud di dalam pangkalan data.';
         }
     }
-
-    
 }
